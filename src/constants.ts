6b81c16ffa3d2b9e68790bdd92acce866a96507f
lafashopcamisas-sudo <lafashopcamisas@gmail.com>
2026-05-08 23:50:23 -0300

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -10,7 +10,7 @@ export const INITIAL_POLLS: Poll[] = [
     createdAt: new Date().toISOString(),
     imageUrl: "https://i.ibb.co/hJTPb8nD/logo-votagora.png%22%20alt=%22logo-votagora",
     options: [
-      { id: "opt-LUIZA", label: "LUIZA", votes: 456, imageUrl: "https://i.ibb.co/zVb0k4wQ/SAI.jpg" },
+      { id: "opt-LUIZA", label: "LUIZA", votes: 456, imageUrl: "https://i.ibb.co/4pPDXLH/avatar.png" },
       { id: "opt-SHEILA", label: "SHEILA", votes: 123, imageUrl: "https://images.unsplash.com/photo-1554151228-14d9def656e4?auto=format&fit=crop&q=80&w=400" },
       { id: "opt-THIAGO", label: "THIAGO", votes: 46, imageUrl: "https://cloudfront-us-east-1.images.arcpublishing.com/newr7/QG5CIEJV6BF6JHVOQLXMGFXLHI.png" },
     ],
